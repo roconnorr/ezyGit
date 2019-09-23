0c8a437679cc9ba3982e1eab5ad832870436f8ae
Maunder <maunderscott@gmail.com>
2019-09-23 17:41:41 +1200

    Cut down example. Removed Semantic and simple-git Adding Blueprint and nodegit

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Button, Label, Dimmer, Loader } from "semantic-ui-react";
-import { PopOver } from "./PopOver";
-import { getStatus } from "./git/git";
-import { StatusResult } from "simple-git/promise";
-import Header from "./components/header";
+import { OmnibarExample } from "./components/OmniBar/OmniBar";
 
 interface IState {
   isLoaded: boolean;
-  gitStatus: StatusResult | null;
 }
 interface IProps {}
 
 class App extends Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
-    this.state = { isLoaded: false, gitStatus: null };
+    this.state = { isLoaded: false };
   }
 
-  async componentDidMount() {
-    this.setState({ isLoaded: false });
-    const status = await getStatus();
-
-    console.log(status);
-    this.setState({ isLoaded: true, gitStatus: status });
-  }
+  async componentDidMount() {}
 
   render() {
-    const { isLoaded, gitStatus } = this.state;
+    const { isLoaded } = this.state;
     return (
       <div className="App">
-        {isLoaded ? (
-          Header(gitStatus!)
-        ) : (
-          <Dimmer inverted active>
-            <Loader />
-          </Dimmer>
-        )}
-        <Button variant="contained">Test asd</Button>
-        <PopOver />
+        <OmnibarExample />
       </div>
     );
   }
