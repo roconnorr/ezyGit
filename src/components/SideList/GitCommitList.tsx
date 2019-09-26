454ed84c41bc085a5321487e4e4a1cce4e2a1792
Maunder <maunderscott@gmail.com>
2019-09-26 21:54:42 +1200

    Added toaster

--- src/components/SideList/GitCommitList.tsx
+++ src/components/SideList/GitCommitList.tsx
@@ -3,15 +3,21 @@ import { GitCommitListItem } from "./GitCommitListItem";
 import { CommitDescriptionWithOid } from "isomorphic-git";
 import Scrollbars from "react-custom-scrollbars";
 import ReactList from "react-list";
+import { AppToaster } from "../Toaster/Toaster";
 
 interface ISideListProps {
   data: Array<CommitDescriptionWithOid>;
 }
 
 class GitCommitList extends React.Component<ISideListProps, {}> {
+  handleListItemClick = (commitOid: number) => {
+    console.log(commitOid);
+    AppToaster.show({ message: "Loading " + commitOid });
+  };
+
   renderGitCommit(index: number, key: number | string) {
     const commit = this.props.data[index];
-    return GitCommitListItem(index, key, commit);
+    return GitCommitListItem(index, key, commit, this.handleListItemClick);
   }
 
   render() {
--- src/components/SideList/GitCommitListItem.tsx
+++ src/components/SideList/GitCommitListItem.tsx
@@ -4,14 +4,19 @@ import { CommitDescriptionWithOid } from "isomorphic-git";
 const GitCommitListItem = (
   index: number,
   key: number | string,
-  commit: CommitDescriptionWithOid
+  commit: CommitDescriptionWithOid,
+  onClickCallback: any
 ): any => {
   const author = commit.author;
   const date = new Date(author.timestamp * 1000 + author.timezoneOffset * 1000);
   return (
-    <div key={key} className={index % 2 ? "historyItemEven" : "historyItemOdd"}>
+    <div
+      key={key}
+      className={index % 2 ? "historyItemEven" : "historyItemOdd"}
+      onClick={() => onClickCallback(commit.oid)}
+    >
       <div className="historyItemContent">{commit.message}</div>
-      <div className="historyItemContent" style={{}}>
+      <div className="historyItemContent">
         <span style={{ width: "50%", display: "inline-block" }}>
           {date.toLocaleDateString("en-UK")}
         </span>
