9d1a61198d234826639e247de2de5f5cbd00635d
Maunder <maunderscott@gmail.com>
2019-09-28 21:29:58 +1200

    Added commas

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-import React, { Component } from 'react'
-import './App.css'
-import { NavBar } from './components/NavBar/NavBar'
-import { CommitDescriptionWithOid } from 'isomorphic-git'
+import React, { Component } from 'react';
+import './App.css';
+import { NavBar } from './components/NavBar/NavBar';
+import { CommitDescriptionWithOid } from 'isomorphic-git';
 import {
   getGitLog,
   getCurrentBranch,
   compareChanges,
   fileChanges,
   getModifiedFiles,
-} from './git/git'
+} from './git/git';
 
-import { GitCommitList } from './components/SideList/GitCommitList'
-import { Intent, Spinner } from '@blueprintjs/core'
-import { MainContentList } from './components/Diff/MainContentList'
-import { NewDiff } from './components/Diff/Diff'
+import { GitCommitList } from './components/SideList/GitCommitList';
+import { Intent, Spinner } from '@blueprintjs/core';
+import { MainContentList } from './components/Diff/MainContentList';
+import { NewDiff } from './components/Diff/Diff';
 
 interface IState {
-  isLoaded: boolean
-  gitLog: Array<CommitDescriptionWithOid> | null
-  gitCurrentBranch: string | undefined
-  gitDiff: Array<fileChanges> | null
-  gitModifiedFiles: Array<fileChanges> | null
+  isLoaded: boolean;
+  gitLog: Array<CommitDescriptionWithOid> | null;
+  gitCurrentBranch: string | undefined;
+  gitDiff: Array<fileChanges> | null;
+  gitModifiedFiles: Array<fileChanges> | null;
 }
 interface IProps {}
 // https://isomorphic-git.org/docs/en/log
 
 class App extends Component<IProps, IState> {
   constructor(props: IProps) {
-    super(props)
+    super(props);
     this.state = {
       isLoaded: false,
       gitLog: null,
       gitCurrentBranch: undefined,
       gitDiff: null,
       gitModifiedFiles: null,
-    }
+    };
   }
 
   async componentDidMount() {
@@ -43,18 +43,18 @@ class App extends Component<IProps, IState> {
         gitLog: values[0],
         isLoaded: true,
         gitCurrentBranch: values[1],
-      })
-    })
+      });
+    });
 
-    const temp = await compareChanges()
-    this.setState({ gitDiff: temp })
+    const temp = await compareChanges();
+    this.setState({ gitDiff: temp });
 
-    console.log(await getModifiedFiles())
+    console.log(await getModifiedFiles());
   }
 
   render() {
-    const { isLoaded, gitLog, gitCurrentBranch, gitDiff } = this.state
-    const temp = this.state.gitDiff ? this.state.gitDiff![0] : null
+    const { isLoaded, gitLog, gitCurrentBranch, gitDiff } = this.state;
+    const temp = this.state.gitDiff ? this.state.gitDiff![0] : null;
     return (
       <div className="App bp3-dark">
         <NavBar branch={gitCurrentBranch!} />
@@ -76,12 +76,12 @@ class App extends Component<IProps, IState> {
                 changedText={temp!.originalState}
               />
             ) : null}
-            {gitDiff ? <MainContentList data={gitDiff} /> : null}
+            {/* {gitDiff ? <MainContentList data={gitDiff} /> : null} */}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
